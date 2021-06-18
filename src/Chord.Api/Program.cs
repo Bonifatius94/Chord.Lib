0b83c177184d17460ebf041a5a7f3141bd987c40
Marco Tröster <marco@troester-gmbh.de>
2021-06-18 03:01:17 +0200

    added ASP.NET Web API project as chord endpoint

---
+++ src/Chord.Api/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Chord.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(x => x.UseStartup<Startup>())
+                .Build().Run();
+        }
+    }
+}
