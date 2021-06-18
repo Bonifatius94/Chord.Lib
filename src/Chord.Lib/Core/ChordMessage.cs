9a0dcf83f6f71021d1ba4d5127ccae2d03d5b203
Marco Tröster <marco@troester-gmbh.de>
2021-06-18 03:00:23 +0200

    beautified code

--- src/Chord.Lib/Core/ChordMessage.cs
+++ src/Chord.Lib/Core/ChordMessage.cs
@@ -22,39 +22,6 @@ namespace Chord.Lib.Core
         Dead
     }
 
-    public interface IChordEndpoint
-    {
-        // summary of remote node features
-        long NodeId { get; set; }
-        string IpAddress { get; set; }
-        string Port { get; set; }
-        ChordHealthStatus State { get; set; }
-    }
-
-    public interface IChordRequestMessage
-    {
-        // core message features
-        ChordRequestType Type { get; set; }
-        long RequesterId { get; set; }
-        long RequestedResourceId { get; set; }
-
-        // additional message features for the join/leave procedure
-        IChordEndpoint NewSuccessor { get; set; }
-        IChordEndpoint NewPredecessor { get; set; }
-    }
-
-    public interface IChordResponseMessage
-    {
-        // core message features
-        IChordEndpoint Responder { get; set; }
-
-        // additional message features for the join/leave procedure
-        bool ReadyForDataCopy { get; set; }
-        bool CommitSuccessful { get; set; }
-        IChordEndpoint Predecessor { get; set; }
-        IEnumerable<IChordEndpoint> FingerTable { get; set; }
-    }
-
     public class ChordEndpoint : IChordEndpoint
     {
         // summary of remote node features
@@ -86,5 +53,38 @@ namespace Chord.Lib.Core
         public bool CommitSuccessful { get; set; }
         public IChordEndpoint Predecessor { get; set; }
         public IEnumerable<IChordEndpoint> FingerTable { get; set; }
+    }
+
+    public interface IChordEndpoint
+    {
+        // summary of remote node features
+        long NodeId { get; set; }
+        string IpAddress { get; set; }
+        string Port { get; set; }
+        ChordHealthStatus State { get; set; }
+    }
+
+    public interface IChordRequestMessage
+    {
+        // core message features
+        ChordRequestType Type { get; set; }
+        long RequesterId { get; set; }
+        long RequestedResourceId { get; set; }
+
+        // additional message features for the join/leave procedure
+        IChordEndpoint NewSuccessor { get; set; }
+        IChordEndpoint NewPredecessor { get; set; }
+    }
+
+    public interface IChordResponseMessage
+    {
+        // core message features
+        IChordEndpoint Responder { get; set; }
+
+        // additional message features for the join/leave procedure
+        bool ReadyForDataCopy { get; set; }
+        bool CommitSuccessful { get; set; }
+        IChordEndpoint Predecessor { get; set; }
+        IEnumerable<IChordEndpoint> FingerTable { get; set; }
     }
 }
