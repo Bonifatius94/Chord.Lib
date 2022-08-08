a490ea0fc9e343fe362fa8f945b98f29ae5d8a64
Marco Tröster <marco@troester-gmbh.de>
2022-08-08 21:46:49 +0200

    introduce chord key struct supporting arithmetics

---
+++ src/Chord.Lib/ChordNodeId.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+namespace Chord.Lib;
+
+public readonly struct ChordKey
+{
+    #region Init
+
+    private static readonly Random rng = new Random();
+
+    public static ChordKey PickRandom()
+        => PickRandom(long.MaxValue);
+
+    public static ChordKey PickRandom(BigInteger maxId)
+        => new ChordKey(
+            id: ((long)rng.Next(int.MinValue, int.MaxValue) << 32)
+            | (long)(uint)rng.Next(int.MinValue, int.MaxValue),
+            maxId: maxId);
+
+    public ChordKey(long id, BigInteger maxId)
+        : this(new BigInteger(id), maxId) {}
+
+    public ChordKey(BigInteger id, BigInteger maxId)
+    {
+        Id = id;
+        this.maxId = maxId;
+    }
+
+    #endregion Init
+
+    private readonly BigInteger maxId;
+    public BigInteger Id { get; }
+
+    #region Arithmetics
+
+    private ChordKey Add(ChordKey other)
+        => new ChordKey(id: restMod(Id + other.Id, maxId), maxId: maxId);
+
+    private ChordKey Sub(ChordKey other)
+        => new ChordKey(id: restMod(Id - other.Id, maxId), maxId: maxId);
+
+    private static BigInteger restMod(BigInteger element, BigInteger classes)
+        => (element % classes + classes) % classes;
+
+    #endregion Arithmetics
+
+    #region Comparison
+
+    public static ChordKey operator +(ChordKey a, ChordKey b) => a.Add(b);
+    public static ChordKey operator -(ChordKey a, ChordKey b) => a.Sub(b);
+    public static bool operator ==(ChordKey a, ChordKey b) => a.Id == b.Id;
+    public static bool operator !=(ChordKey a, ChordKey b) => a.Id != b.Id;
+    public static bool operator <(ChordKey a, ChordKey b) => a.Id < b.Id;
+    public static bool operator >(ChordKey a, ChordKey b) => a.Id > b.Id;
+    public static bool operator <=(ChordKey a, ChordKey b) => a.Id <= b.Id;
+    public static bool operator >=(ChordKey a, ChordKey b) => a.Id >= b.Id;
+
+    public override bool Equals([NotNullWhen(true)] object obj)
+        => obj?.GetType() == typeof(ChordKey) && ((ChordKey)obj).Id == Id;
+
+    public override int GetHashCode() => (int)Id;
+
+    #endregion Comparison
+}
