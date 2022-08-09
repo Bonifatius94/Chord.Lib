917d1f5617a4764282561bd0fa0fe9f401af05a9
Marco Tröster <marco@troester-gmbh.de>
2022-08-09 12:18:42 +0200

    beautify, remove constructor taking id as long

--- src/Chord.Lib/ChordKey.cs
+++ src/Chord.Lib/ChordKey.cs
@@ -4,43 +4,61 @@ using System.Numerics;
 
 namespace Chord.Lib;
 
+/// <summary>
+/// An immutable BigInteger key implementation supporting
+/// the residue field arithmetics required for Chord.
+/// </summary>
 public readonly struct ChordKey
 {
     #region Init
 
     private static readonly Random rng = new Random();
 
+    /// <summary>
+    /// Pick a random key within [0, 2^64).
+    /// </summary>
+    /// <returns>a new key instance with random id</returns>
     public static ChordKey PickRandom()
-        => PickRandom(long.MaxValue);
+        => PickRandom(ulong.MaxValue);
 
-    public static ChordKey PickRandom(BigInteger maxId)
+    /// <summary>
+    /// Pick a random key within [0, keySpace).
+    /// </summary>
+    /// <param name="keySpace">The number of possible keys to pick from.</param>
+    /// <returns>a new key instance with random id</returns>
+    public static ChordKey PickRandom(BigInteger keySpace)
     {
-        var newId = restMod(((long)rng.Next(int.MinValue, int.MaxValue) << 31)
-            | (long)(uint)rng.Next(int.MinValue, int.MaxValue), maxId);
-        return new ChordKey(newId, maxId);
+        // TODO: this only produces keys within [0, 2^64) mod keySpace
+        //       -> make this really expand into the BigInteger space within [0, keySpace)
+        var newId = restMod(((ulong)rng.Next(int.MinValue, int.MaxValue) << 31)
+            | (ulong)(uint)rng.Next(int.MinValue, int.MaxValue), keySpace);
+        return new ChordKey(newId, keySpace);
     }
 
-    public ChordKey(long id, BigInteger maxId)
-        : this(new BigInteger(id), maxId) {}
-
-    public ChordKey(BigInteger id, BigInteger maxId)
+    /// <summary>
+    /// Create a key with the given residue field identity
+    /// according to the given key space.
+    /// </summary>
+    /// <param name="id">The residue field identity that the new key represents.</param>
+    /// <param name="keySpace">The upper bound of the residue field.</param>
+    public ChordKey(BigInteger id, BigInteger keySpace)
     {
-        Id = restMod(id, maxId);
-        this.maxId = maxId;
+        Id = restMod(id, keySpace);
+        this.keySpace = keySpace;
     }
 
     #endregion Init
 
-    private readonly BigInteger maxId;
+    private readonly BigInteger keySpace;
     public BigInteger Id { get; }
 
     #region Arithmetics
 
-    private ChordKey Add(ChordKey other)
-        => new ChordKey(id: restMod(Id + other.Id, maxId), maxId: maxId);
+    private ChordKey add(ChordKey other)
+        => new ChordKey(id: restMod(Id + other.Id, keySpace), keySpace: keySpace);
 
-    private ChordKey Sub(ChordKey other)
-        => new ChordKey(id: restMod(Id - other.Id, maxId), maxId: maxId);
+    private ChordKey sub(ChordKey other)
+        => new ChordKey(id: restMod(Id - other.Id, keySpace), keySpace: keySpace);
 
     private static BigInteger restMod(BigInteger element, BigInteger classes)
         => (element % classes + classes) % classes;
@@ -49,8 +67,8 @@ public readonly struct ChordKey
 
     #region Comparison
 
-    public static ChordKey operator +(ChordKey a, ChordKey b) => a.Add(b);
-    public static ChordKey operator -(ChordKey a, ChordKey b) => a.Sub(b);
+    public static ChordKey operator +(ChordKey a, ChordKey b) => a.add(b);
+    public static ChordKey operator -(ChordKey a, ChordKey b) => a.sub(b);
     public static bool operator ==(ChordKey a, ChordKey b) => a.Id == b.Id;
     public static bool operator !=(ChordKey a, ChordKey b) => a.Id != b.Id;
     public static bool operator <(ChordKey a, ChordKey b) => a.Id < b.Id;
@@ -61,7 +79,7 @@ public readonly struct ChordKey
     public override bool Equals([NotNullWhen(true)] object obj)
         => obj?.GetType() == typeof(ChordKey) && ((ChordKey)obj).Id == Id;
 
-    public override int GetHashCode() => (int)(Id % int.MaxValue);
+    public override int GetHashCode() => 0;
 
     #endregion Comparison
 }
