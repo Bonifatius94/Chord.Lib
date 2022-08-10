bad45912eedc4090c6514778905d2ffb823e2bfa
Marco Tröster <marco@troester-gmbh.de>
2022-08-11 01:04:25 +0200

    add tests for bootstrap error handling

--- src/Chord.Lib.Test/ChordBootstrapTest.cs
+++ src/Chord.Lib.Test/ChordBootstrapTest.cs
@@ -11,9 +11,9 @@ namespace Chord.Lib.Test.BootstrapperTest;
 
 public class BootstrapperTest
 {
-    class RequestSenderMock : IChordClient
+    class SpecificSuccessfulPingRequestSenderMock : IChordClient
     {
-        public RequestSenderMock(string successIp)
+        public SpecificSuccessfulPingRequestSenderMock(string successIp)
             => this.successIp = successIp;
 
         private string successIp;
@@ -26,6 +26,26 @@ public class BootstrapperTest
 
             await Task.Delay(2000);
             throw new TimeoutException($"request for { receiver } timed out!");
+        }
+    }
+
+    class AllPingsTimeoutRequestSenderMock : IChordClient
+    {
+        public async Task<IChordResponseMessage> SendRequest(
+            IChordRequestMessage request, IChordEndpoint receiver, CancellationToken? token = null)
+        {
+            await Task.Delay(2000);
+            throw new TimeoutException($"request for { receiver } timed out!");
+        }
+    }
+
+    class AllPingsThrowRequestSenderMock : IChordClient
+    {
+        public async Task<IChordResponseMessage> SendRequest(
+            IChordRequestMessage request, IChordEndpoint receiver, CancellationToken? token = null)
+        {
+            await Task.Delay(20);
+            throw new TimeoutException($"DNS error for { receiver }!");
         }
     }
 
@@ -44,7 +64,7 @@ public class BootstrapperTest
         ipConfigMock.IPv4Broadcast.Returns(IPAddress.Parse("192.168.178.255"));
 
         string expBootstrapIp = "192.168.178.243";
-        var senderMock = new RequestSenderMock(expBootstrapIp);
+        var senderMock = new SpecificSuccessfulPingRequestSenderMock(expBootstrapIp);
 
         var endpointGen = new IPv4EndpointGenerator(
             ipConfigMock, (k) => new ChordKey(k, 254));
@@ -52,5 +72,39 @@ public class BootstrapperTest
         var bootstrapNode = await sut.FindBootstrapNode();
 
         bootstrapNode.IpAddress.Should().Be(expBootstrapIp);
+    }
+
+    [Fact]
+    public async Task Test_ShouldFindNoBootstrapNode_WhenAllPingsTimeOut()
+    {
+        ipConfigMock.ChordPort.Returns(9876);
+        ipConfigMock.IPv4NetworkId.Returns(IPAddress.Parse("192.168.178.0"));
+        ipConfigMock.IPv4Broadcast.Returns(IPAddress.Parse("192.168.178.255"));
+
+        var senderMock = new AllPingsTimeoutRequestSenderMock();
+
+        var endpointGen = new IPv4EndpointGenerator(
+            ipConfigMock, (k) => new ChordKey(k, 254));
+        var sut = new ChordBootstrapper(senderMock, endpointGen);
+        var bootstrapNode = await sut.FindBootstrapNode();
+
+        bootstrapNode.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task Test_ShouldFindNoBootstrapNode_WhenAllPingsThrowAnException()
+    {
+        ipConfigMock.ChordPort.Returns(9876);
+        ipConfigMock.IPv4NetworkId.Returns(IPAddress.Parse("192.168.178.0"));
+        ipConfigMock.IPv4Broadcast.Returns(IPAddress.Parse("192.168.178.255"));
+
+        var senderMock = new AllPingsThrowRequestSenderMock();
+
+        var endpointGen = new IPv4EndpointGenerator(
+            ipConfigMock, (k) => new ChordKey(k, 254));
+        var sut = new ChordBootstrapper(senderMock, endpointGen);
+        var bootstrapNode = await sut.FindBootstrapNode();
+
+        bootstrapNode.Should().BeNull();
     }
 }
