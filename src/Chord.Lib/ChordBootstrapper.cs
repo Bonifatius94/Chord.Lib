9e15de7ead51bfceafaa34f09e3cefe3b94759f4
Marco Tröster <marco@troester-gmbh.de>
2022-08-10 02:15:49 +0200

    move IPv4 endpoint generator to Impl namespace

--- src/Chord.Lib/ChordBootstrapper.cs
+++ src/Chord.Lib/ChordBootstrapper.cs
@@ -1,71 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chord.Lib;
 
-public interface IExplorableChordEndpointGenerator
-{
-    IEnumerable<IChordEndpoint> GenerateEndpoints();
-}
-
-public class IPv4EndpointGenerator : IExplorableChordEndpointGenerator
-{
-    public IPv4EndpointGenerator(
-        IIpSettings ipConfig,
-        Func<BigInteger, ChordKey> newKey)
-    {
-        this.ipConfig = ipConfig;
-        this.newKey = newKey;
-    }
-
-    private readonly IIpSettings ipConfig;
-    private readonly Func<BigInteger, ChordKey> newKey;
-
-    private (BigInteger, BigInteger) getFirstAndLastAddress()
-    {
-        // expect all chord nodes to use the same port
-        // expect something like an exclusive LAN for chord nodes
-
-        // get network id and broadcast address
-        var networkId = ipConfig.Ipv4NetworkId;
-        var broadcast = ipConfig.Ipv4Broadcast;
-
-        // determine the first and last address in the address space
-        var firstIp = new BigInteger(networkId.GetAddressBytes(), isUnsigned: true) + 1;
-        var lastIp = new BigInteger(broadcast.GetAddressBytes(), isUnsigned: true) - 1;
-        return (firstIp, lastIp);
-    }
-
-    public IEnumerable<IChordEndpoint> GenerateEndpoints()
-    {
-        var chordPort = ipConfig.ChordPort;
-        var (firstIp, lastIp) = getFirstAndLastAddress();
-        var allEndpoints = BigIntEnumerable.Range(firstIp, lastIp)
-            .Select(addr => new ChordEndpoint() {
-                NodeId = newKey(-1),
-                IpAddress = new IPAddress(addr.ToByteArray()).ToString(),
-                Port = chordPort.ToString()
-            });
-        return allEndpoints;
-    }
-}
-
 public class ChordBootstrapper : IChordBootstrapper
 {
     public ChordBootstrapper(
+        IChordClient sender,
         IExplorableChordEndpointGenerator endpointGenerator)
     {
+        this.sender = sender;
         this.endpointGenerator = endpointGenerator;
     }
 
+    private IChordClient sender;
     private IExplorableChordEndpointGenerator endpointGenerator;
 
-    public async Task<IChordEndpoint> FindBootstrapNode(IChordRequestSender sender)
+    public async Task<IChordEndpoint> FindBootstrapNode()
     {
         const int PING_TIMEOUT_MS = 1000;
         const int NUM_PARALLEL_PINGS = 128;
@@ -94,7 +49,7 @@ public class ChordBootstrapper : IChordBootstrapper
     }
 
     private bool pingEndpoint(
-        IChordRequestSender sender,
+        IChordClient sender,
         IChordEndpoint endpoint,
         int timeoutMillis)
     {
---
+++ src/Chord.Lib/Impl/IPv4EndpointGenerator.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+
+namespace Chord.Lib.Impl;
+
+public class IPv4EndpointGenerator : IExplorableChordEndpointGenerator
+{
+    public IPv4EndpointGenerator(
+        IIpSettings ipConfig,
+        Func<BigInteger, ChordKey> newKey)
+    {
+        this.ipConfig = ipConfig;
+        this.newKey = newKey;
+    }
+
+    private readonly IIpSettings ipConfig;
+    private readonly Func<BigInteger, ChordKey> newKey;
+
+    private (BigInteger, BigInteger) getFirstAndLastAddress()
+    {
+        // expect all chord nodes to use the same port
+        // expect something like an exclusive LAN for chord nodes
+
+        // get network id and broadcast address
+        var networkId = ipConfig.Ipv4NetworkId;
+        var broadcast = ipConfig.Ipv4Broadcast;
+
+        // determine the first and last address in the address space
+        var firstIp = new BigInteger(networkId.GetAddressBytes(), isUnsigned: true) + 1;
+        var lastIp = new BigInteger(broadcast.GetAddressBytes(), isUnsigned: true) - 1;
+        return (firstIp, lastIp);
+    }
+
+    public IEnumerable<IChordEndpoint> GenerateEndpoints()
+    {
+        var chordPort = ipConfig.ChordPort;
+        var (firstIp, lastIp) = getFirstAndLastAddress();
+        var allEndpoints = BigIntEnumerable.Range(firstIp, lastIp)
+            .Select(addr => new ChordEndpoint() {
+                NodeId = newKey(-1),
+                IpAddress = new IPAddress(addr.ToByteArray()).ToString(),
+                Port = chordPort.ToString()
+            });
+        return allEndpoints;
+    }
+}
