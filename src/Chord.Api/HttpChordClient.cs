d6586a5209bbd9c53e01a29a196ef4f7167bf182
Marco Tröster <marco@troester-gmbh.de>
2022-08-19 17:58:32 +0200

    transform Chord.Api to a minimal api

--- src/Chord.Api/HttpChordClient.cs
+++ src/Chord.Api/HttpChordClient.cs
@@ -4,30 +4,25 @@ using System.Threading;
 using System.Threading.Tasks;
 using Chord.Lib;
 
-namespace Chord.Api.Controllers;
+namespace Chord.Api;
 
-public class HttpChordRequestSender : IChordClient
+public class HttpChordClient : IChordClient
 {
     public async Task<IChordResponseMessage> SendRequest(
-        IChordRequestMessage request, IChordEndpoint receiver, CancellationToken? token)
+        IChordRequestMessage request, IChordEndpoint receiver, CancellationToken token)
     {
-        // create the request URL to the remote chord endpoint
         string url = $"http://{ receiver.IpAddress }:{ receiver.Port }/chord";
-
-        // serialize the request as JSON
         string requestJson = JsonSerializer.Serialize(request); // TODO: check if this works
 
-        // open a HTTP connection to the remote endpoint
         using (var client = new HttpClient())
         using (var content = new StringContent(requestJson))
         {
             // send the request as JSON, parse the response from JSON
-            var httpResponse = await client.PostAsync(url, content);
+            var httpResponse = await client.PostAsync(url, content, token);
             var responseJson = await httpResponse.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<ChordResponseMessage>(responseJson);
         }
 
         // TODO: keep the TCP connections open as long as possible
-        // TODO: add error handling for timeouts, etc.
     }
 }
--- src/Chord.Api/Program.cs
+++ src/Chord.Api/Program.cs
@@ -1,21 +1,48 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Chord.Api;
+using Chord.Config;
+using Chord.Lib;
+using Chord.Lib.Impl;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Chord.Api
+var app = WebApplication.Create();
+var node = await ConfigureChord();
+app.MapPost("/", async ([FromBody] ChordRequestMessage request, CancellationToken token)
+    => new OkObjectResult((ChordResponseMessage)await node.ProcessRequest(request, token)));
+app.Run();
+
+async Task<ChordNode> ConfigureChord()
 {
-    public class Program
+    const long keySpace = long.MaxValue;
+
+    var ipConfig = new IpSettings();
+    var localEndpoint = new IPv4Endpoint(
+        ChordKey.PickRandom(keySpace),
+        ChordHealthStatus.Starting,
+        ipConfig.ChordIpv4Address.ToString(),
+        ipConfig.ChordPort.ToString());
+
+    var httpClient = new HttpChordClient();
+    var endpointGenerator = new IPv4EndpointGenerator(
+        ipConfig, (key) => new ChordKey(key, keySpace));
+    var bootstrapper = new ChordBootstrapper(endpointGenerator);
+
+    // TODO: replace with real worker
+    var payloadWorker = new ZeroProtocolPayloadWorker();
+    var nodeConfig = new ChordNodeConfiguration();
+    var node = new ChordNode(localEndpoint, httpClient, payloadWorker, nodeConfig);
+
+    var cancelCallback = new CancellationTokenSource();
+    int timeoutMillis = 100;
+    while (node.NodeState != ChordHealthStatus.Idle)
     {
-        public static void Main(string[] args)
-        {
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(x => x.UseStartup<Startup>())
-                .Build().Run();
-        }
+        var joinTask = node.JoinNetwork(bootstrapper, cancelCallback.Token);
+        await joinTask.Timeout(timeoutMillis, cancelCallback.Token);
+        await Task.Delay(timeoutMillis);
+        timeoutMillis *= 2;
     }
+
+    return node;
 }
