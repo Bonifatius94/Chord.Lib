9e70e5edc316bc0620a1c86f3da2d25a198f2cc8
Marco Tröster <marco@troester-gmbh.de>
2022-08-21 15:49:21 +0200

    add network join test case for debugging

---
+++ src/Chord.Lib.Test/ChordNetworkJoinTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Chord.Lib.Impl;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Chord.Lib.Test;
+
+public class SingleNodeJoinTest
+{
+    const long KEY_SPACE = long.MaxValue;
+
+    private IEnumerable<IChordEndpoint> GenerateEndpoints(int count)
+        => Enumerable.Range(1, count)
+            .Select(i => $"10.0.0.{i}")
+            .Select(ip => new IPv4Endpoint(ip, "9000", KEY_SPACE));
+
+    [Fact]
+    public async Task JoinInitialTwoNodes()
+    {
+        int numNodes = 2;
+        var httpClient = new IPv4NetworkMock();
+        var endpoints = GenerateEndpoints(numNodes).ToList();
+        var bootstrapper = Substitute.For<IChordBootstrapper>();
+        bootstrapper.FindBootstrapNode(default, default)
+            .ReturnsForAnyArgs(args => endpoints[1]);
+
+        // TODO: replace with real worker
+        var payloadWorker = new ZeroProtocolPayloadWorker();
+        var nodeConfig = new ChordNodeConfiguration();
+        var node1 = new ChordNode(endpoints[0], httpClient, payloadWorker, nodeConfig);
+        var node2 = new ChordNode(endpoints[1], httpClient, payloadWorker, nodeConfig);
+        httpClient.Nodes = new List<ChordNode>{ node1, node2 };
+
+        var cancelCallback = new CancellationTokenSource();
+        await node1.JoinNetwork(bootstrapper, cancelCallback.Token);
+
+        node1.Successor.NodeId.Should().BeEquivalentTo(node2.Local.NodeId);
+        node1.Predecessor.NodeId.Should().BeEquivalentTo(node2.Local.NodeId);
+        node2.Successor.NodeId.Should().BeEquivalentTo(node1.Local.NodeId);
+        node2.Predecessor.NodeId.Should().BeEquivalentTo(node1.Local.NodeId);
+    }
+}
